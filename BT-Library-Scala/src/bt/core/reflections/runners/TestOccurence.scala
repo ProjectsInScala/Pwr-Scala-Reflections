1581c4f5633af9ab41c20b800236e2b5a7602ea7
korytkoBT <209826@student.pwr.wroc.pl>
2016-01-06 13:56:30 +0000

    T1: ProgPar, l10, Ex2 works, all works, comming back my previous colours

--- BT-Library-Scala/src/bt/core/reflections/runners/TestOccurence.scala
+++ BT-Library-Scala/src/bt/core/reflections/runners/TestOccurence.scala
@@ -5,6 +5,11 @@ object TestOccurence {
   def main(args: Array[String]){
     
     var samplePoint: Point = new Point(3,4)
+    println("Exercise 1")
     samplePoint.debugName
+    println()
+    
+    println("Exercise 2")
+    samplePoint.debugVars
   }
 }
