272d7b712e12f50cf54ba37a8f792fa422ada79b
korytkoBT <209826@student.pwr.wroc.pl>
2016-01-06 13:11:57 +0000

    T1: ProgParad, l10, Ex1, Works

---
+++ BT-Library-Scala/src/bt/core/reflections/elementary/Point.scala
@@ -0,0 +1,8 @@
+package bt.core.reflections.elementary
+import bt.core.reflections.traits.Debug
+
+class Point(xv: Int, yv: Int) extends Debug {
+    var x: Int = xv
+    var y: Int = yv
+    var a: String = "test"
+}
